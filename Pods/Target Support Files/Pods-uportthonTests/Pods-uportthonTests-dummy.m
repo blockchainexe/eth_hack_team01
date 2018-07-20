8d3ae19f6645108142f4249d736c0c73711a80c9
iida-hayato <hayato.iida.0213@gmail.com>
2018-07-20 22:38:46 +0900

    init

---
+++ Pods/Target Support Files/Pods-uportthonTests/Pods-uportthonTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_uportthonTests : NSObject
+@end
+@implementation PodsDummy_Pods_uportthonTests
+@end
---
+++ uportthon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uportthon
+//
+//  Created by hayato.iida on 2018/07/20.
+//  Copyright © 2018年 hayato.iida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
